d64f425abb67d9465d526091c6adf10c84e23aa3
Gabriel Soares <gabrielsoacc@gmail.com>
2026-03-11 08:48:33 -0300

    feat: added profitCalculator function

--- investment_calculator.go
+++ investment_calculator.go
@@ -9,15 +9,31 @@ func calculateInvestment (
 	investmentAmount float64,
 	expectedReturnRate float64,
 	periodInYears float64) {
+		
+		const inflationRate float64 = 2.5;
+		
+		futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, periodInYears)
+		futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, periodInYears);
+		
+		fmt.Printf("investment amount: %.2f\n", investmentAmount);
+		fmt.Printf("Future value: %.2f\n", futureValue);
+		fmt.Printf("Future value with inflationRate: %.2f\n", futureRealValue);
+		println("####### END #########")
+}
 
-	const inflationRate float64 = 2.5;
+func profitCalculator(
+	revenue float64,
+	expenses float64,
+	taxRate float64) {
 
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, periodInYears)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, periodInYears);
-	
-	fmt.Printf("investment amount: %.2f\n", investmentAmount);
-	fmt.Printf("Future value: %.2f\n", futureValue);
-	fmt.Printf("Future value with inflationRate: %.2f\n", futureRealValue);
+	var ebt float64 = revenue - expenses;
+	var profit = ebt * (1 - taxRate / 100 );
+	var ratio = ebt / profit;
+
+	fmt.Printf("Your ebt: %.2f\n", ebt);
+	fmt.Printf("Your profit: %.2f\n", profit);
+	fmt.Printf("Your ratio: %.2f\n", ratio);
+	println("####### END #########")
 }
 
 func main() {
@@ -27,9 +43,24 @@ func main() {
 		fmt.Println("(1) Profit Calculator");
 		fmt.Println("(2) Investment Calculator");
 		fmt.Println("(3) Exit");
-
+		
 		fmt.Print("Enter your choice: ");
 		fmt.Scan(&choice);
+
+		if(choice == 1){
+			var renenue, expenses, taxRate float64;
+			
+			fmt.Print("Enter your revenue: ")
+			fmt.Scan(&renenue);
+			
+			fmt.Print("Enter your expenses: ");
+			fmt.Scan(&expenses);
+			
+			fmt.Print("Enter the tax rate: ");
+			fmt.Scan(&taxRate);
+			
+			profitCalculator(renenue, expenses, taxRate);
+		}
 
 		if(choice == 2){
 			checkMark := rune(0x2705);
@@ -54,7 +85,4 @@ func main() {
 			println("Goodbye :)");
 		}
 	}
-	
-
-	
 }
