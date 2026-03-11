f383dd70f5b0fe6aff2c42a8c70fb8c399fe7590
Gabriel Soares <gabrielsoacc@gmail.com>
2026-03-11 08:19:18 -0300

    feat: created function for calculate investment

--- investment_calculator.go
+++ investment_calculator.go
@@ -5,25 +5,37 @@ import (
 	"math"
 )
 
-func main() {
+func calculateInvestment (
+	investmentAmount float64,
+	expectedReturnRate float64,
+	periodInYears float64) {
+
 	const inflationRate float64 = 2.5;
-	investmentAmount, expectedReturnRate, years := 0.0, 0.0, 0.0;
-	checkMark := rune(0x2705);
 
-	fmt.Print("Enter your invesmentAmount: ")
-	fmt.Scan(&investmentAmount);
-
-	fmt.Print("Enter yout expected return rate, example: '5.5': ");
-	fmt.Scan(&expectedReturnRate);
-
-	fmt.Print("Enter the period in years: ");
-	fmt.Scan(&years);
-
-	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
-	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years);
+	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, periodInYears)
+	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, periodInYears);
 	
 	fmt.Printf("investment amount: %.2f\n", investmentAmount);
 	fmt.Printf("Future value: %.2f\n", futureValue);
 	fmt.Printf("Future value with inflationRate: %.2f\n", futureRealValue);
+}
+
+func main() {
+	checkMark := rune(0x2705);
+	investmentAmount, expectedReturnRate, periodInYears := 0.0, 0.0, 0.0;
+	
+	fmt.Print("Enter your invesmentAmount: ")
+	fmt.Scan(&investmentAmount);
+	
+	fmt.Print("Enter yout expected return rate, example: '5.5': ");
+	fmt.Scan(&expectedReturnRate);
+	
+	fmt.Print("Enter the period in periodInYears: ");
+	fmt.Scan(&periodInYears);
+
+	calculateInvestment(investmentAmount, expectedReturnRate, periodInYears);
+
 	fmt.Println(string(checkMark));
+
+	
 }
