3969e0b2e54133c978a43096bb0258175e50c608
Gabriel Soares <gabrielsoacc@gmail.com>
2026-03-11 08:13:24 -0300

    feat: added input values by ternminal I/O

--- investment_calculator.go
+++ investment_calculator.go
@@ -7,13 +7,23 @@ import (
 
 func main() {
 	const inflationRate float64 = 2.5;
-	investmentAmount, expectedReturnRate, years := 1000.0, 5.5, 10.0;
+	investmentAmount, expectedReturnRate, years := 0.0, 0.0, 0.0;
 	checkMark := rune(0x2705);
+
+	fmt.Print("Enter your invesmentAmount: ")
+	fmt.Scan(&investmentAmount);
+
+	fmt.Print("Enter yout expected return rate, example: '5.5': ");
+	fmt.Scan(&expectedReturnRate);
+
+	fmt.Print("Enter the period in years: ");
+	fmt.Scan(&years);
 
 	futureValue := investmentAmount * math.Pow(1 + expectedReturnRate / 100, years)
 	futureRealValue := futureValue / math.Pow(1 + inflationRate / 100, years);
 	
-	fmt.Printf("%.2f\n", futureValue);
+	fmt.Printf("investment amount: %.2f\n", investmentAmount);
+	fmt.Printf("Future value: %.2f\n", futureValue);
+	fmt.Printf("Future value with inflationRate: %.2f\n", futureRealValue);
 	fmt.Println(string(checkMark));
-	fmt.Printf("%.2f\n", futureRealValue);
 }
