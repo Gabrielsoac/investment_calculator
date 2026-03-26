f1aabab9bfce352402f2da670f3d321789f8bd1b
Gabriel Soares <gabrielsoacc@gmail.com>
2026-03-25 22:14:51 -0300

    feat: added random data third package

--- menu.go
+++ menu.go
@@ -1,9 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Pallinder/go-randomdata"
+)
 
 func showMenu () {
 	fmt.Println("\n####### MENU #######");
+	fmt.Println("Contact Us:" + randomdata.PhoneNumber())
+
+
 	fmt.Println("(1) Profit Calculator");
 	fmt.Println("(2) Investment Calculator");
 	fmt.Println("(3) Exit");
